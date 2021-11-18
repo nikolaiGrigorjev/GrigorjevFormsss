c9b8a66c3bf8a6eb55ed6fb7736b0c3bec89b824
Opilane TTHK <opilane@tthk.ee>
2021-11-18 11:41:03 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrigorjevFormsss
+{
+    public partial class Form1 : Form
+    {
+        TreeView tree;
+        Button btn;
+        Label lbl;
+        PictureBox pctBox;
+        pubclic int count = 1;
+        public Form1()
+        {
+            this.Height = 500;
+            this.Width = 700;
+            this.Text = "NBA Forms";
+            Image img = new Bitmap(@"C:\Users\opilane\source\repos\GrigorjevFORM\GrigorjevFormsss\artworks-ukQRhAYBpFcvtoAF-sYbxfw-t500x500.jpg");
+            this.BackgroundImage = img;
+            tree = new TreeView();
+            tree.Dock = DockStyle.Left;
+            tree.AfterSelect += Tree_AfterSelect;
+            TreeNode tn = new TreeNode("Elemendid");
+            tn.Nodes.Add(new TreeNode("Nupp"));
+            tn.Nodes.Add(new TreeNode("Silt-label"));
+            tn.Nodes.Add(new TreeNode("Märkeruut-CheckBox"));
+            tn.Nodes.Add(new TreeNode("Radionupp-Radiobutton"));
+            tn.Nodes.Add(new TreeNode("Tekstkast-TextBox"));
+            tn.Nodes.Add(new TreeNode("PictureBox"));
+            tn.Nodes.Add(new TreeNode("Kaart-TabControl"));
+            tn.Nodes.Add(new TreeNode("MessageBox"));
+            
+
+
+
+
+            //nupp
+            btn = new Button();
+            btn.Text = "Vajuta siia";
+            btn.Location = new Point(100, 50);
+            btn.Height = 20;
+            btn.Width = 100;
+            btn.Click += Btn_Click;
+            //pealkiri
+            lbl = new Label();
+            lbl.Text = "Elementide loomine c# abi";
+            lbl.Size = new Size(600, 30);
+            lbl.Location = new Point(150, 0);
+            lbl.MouseHover += Lbl_MouseHover;
+            lbl.MouseLeave += Lbl_MouseLeave;
+            //picturebox
+
+            pctBox = new PictureBox();
+            pctBox.Size = new Size(300, 170);
+            pctBox.Location = new Point(150, 100);
+
+            pctBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctBox.Image = Image.FromFile(@"C:\Users\opilane\source\repos\GrigorjevFORM\GrigorjevFormsss\el-primo-brawl-stars-guide.jpg");
+
+            
+
+
+
+
+            tree.Nodes.Add(tn);
+            this.Controls.Add(tree);
+
+            
+        }
+
+        private void Lbl_MouseLeave(object sender, EventArgs e)
+        {
+            lbl.BackColor = Color.Transparent;
+        }
+
+        private void Lbl_MouseHover(object sender, EventArgs e)
+        {
+            lbl.BackColor = Color.FromArgb(200, 10, 20);
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void Tree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text=="Nupp")
+            {
+                this.Controls.Add(btn);
+            }
+            else if(e.Node.Text=="Silt-label")
+            {
+                this.Controls.Add(lbl);
+            }
+            else if (e.Node.Text == "PictureBox")
+            {
+                this.Controls.Add(pctBox);
+            }
+
+        }
+
+
+    }
+}
